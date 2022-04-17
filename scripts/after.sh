ac51c51d1db74fb6785256b7dc8447d0a9b49a14
kalyan kalapala <58908209+kalyankalapala@users.noreply.github.com>
2022-04-17 15:52:38 -0500

    sa

--- scripts/after.sh
+++ scripts/after.sh
@@ -13,7 +13,7 @@ ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
 #### docker-compose up ####
-cd /home/developer/dockernodejs
+cd /home/ec2-user/dockernodejs
 sudo docker-compose down
 sudo docker rmi -f docker-nodejs
 sudo docker-compose up -d
